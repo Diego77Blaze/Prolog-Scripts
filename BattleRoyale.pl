0c6784e84d4422ad742f2fb78d16037ac960b78f
Diego77Blaze <48413305+Diego77Blaze@users.noreply.github.com>
2020-02-19 18:26:30 +0100

    Content updated

--- BattleRoyale.pl
+++ BattleRoyale.pl
@@ -1,29 +1,35 @@
 % Autor:
 % Fecha: 13/02/2020
 
-%Soldados se atacan entre sí
-%Lista de todos los soldados
-%Elegir soldado atacante y soldado receptor
-%Moificar la vida del defensor tras el ataque
-%Vida <= 0--> sacar al soldado de la lista
-%lista.suze = 1 --> Winner winner chicken dinner Victoria Royale
-%Mostrar mensajes de la situacion
-
-
 %Nombre Vida Daño
+%Soldados se atacan entre sí
+%Lista con todos los solodados
+%Elegir soldado atacante y soldado defensor
+%Modificar la vida del defensor tras el ataque
+%Vida <= 0 --> Sacar soldado de la lista
+%Lista.size = 1 --> Terminar
+%Mostrar mensajes de la situación
+
 soldado(['john',15,1]).
 soldado(['peter',10,2]).
 soldado(['ryan',5,3]).
 
+%Sacar valor aleatorio --> random(2,9,X).
+
 crear_lista(ListaAux, ListaOut):-
-    soldado(X),
-    \+(member(X, ListaAux)), !,
-    append(ListaAux, [X], ListaAux2),
-    crear_lista(ListaAux2,ListaOut).
-crear_lista(ListaOut ,ListaOut).
+     soldado(X),
+     \+(member(X, ListaAux)), !,
+     append(ListaAux, [X], ListaAux2),
+     crear_lista(ListaAux2, ListaOut).
+
+crear_lista(ListaOut, ListaOut).
 
 atacar(Atacante,Defensor,DefensorOut):-
-    Atacante[NAtacante,VAtacante,DAtacante],
-    Defensor=[NDefensor,VDefensor,DDefensor],
-    substract(VDefensor, DAtacante, VDefensorOut),
-    DefensorOut=[NDefensor,VDefensorOut,DDefensor].
+     Atacante=[NAtacante,VAtacante,DAtacante],
+     Defensor=[NDefensor,VDefensor,DDefensor],
+     VDefensorOut is VDefensor - DAtacante,
+     DefensorOut = [NDefensor, VDefensorOut, DDefensor].
+
+select_atacante_defensor(ListaSoldados, DatosAtacante, DatosDefensor):-
+    repeat,
+    random(0,2,NumAtacante), random(0,2,NumDefensor), \+ (NumAtacante = NumDefensor)
